bda5a1c04a372ae7a0afec7468e1487f3951ef0f
kefarova-png <kefarova@gmail.com>
2026-03-27 20:18:48 +0400

    Версия с функциями

--- text_analyzing.py
+++ text_analyzing.py
@@ -1,40 +1,50 @@
-original_text = input("Введите текст для анализа: ")
-main_symbols = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
+def input_text_filter(): #  Ввод текста и предварительная фильтрация
+    original_text = input("Введите текст для анализа: ")
+    #  "Лишние" символы, которые не будем считать при анализе
+    not_text = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+    #  Собираем текст без "лишних" символов; переводим в нижний регистр
+    clean_text = ''.join(ch for ch in original_text if ch not in not_text or ch == ' ').lower()
+    return clean_text
 
-clean_text = "" #  Пока пустая переменная-текст для сборки в неё отфильтрованных символов
-for char in original_text:
-    if char in main_symbols:
-        clean_text += char.lower() #  Собираем текст только из цифр, пробелов и переведённых в нижний регистр букв
 
-words = clean_text.split() #  Перевод фильтрованного текста в list-объект, т.е. список слов/чисел
-#  print(words) #  строка для контроля
+def word_count(clean_text): #  Считаем количество слов, ориентируясь на прбелы-разделители
+    words = clean_text.split() #  Список слов
+    count = len(words)
+    print("1. Количество слов/чисел:", count)
+    return words
 
-count = 0 #  1. Переменная-счётчик количества слов/чисел в тексте по списку слов
-for element in words:
-    count += 1
-print("1. Количество слов/чисел:", count)
 
-max_lenght = 0 #  Переменная для хранения текущей максимальной длины
-biggest_word = "" # Переменная для хранения текущего максимального длинного слова
-for element in words:
-    if len(element) > max_lenght:
-        max_lenght = len(element)
-        biggest_word = element #  2. Первое самое длинное слово/число в тексте:
-print("2. Первое самое длинное слово/число:", biggest_word)
+def longest_word_finding(words):
+    biggest_word = "" #  Переменная для хранения текущего максимального длинного слова
+    for element in words:
+        if len(element) > len(biggest_word):
+            biggest_word = element
+    print("2. Первое самое длинное слово/число:", biggest_word)
 
-vowels = "аеёиоуыэюя"
-count_vowels = 0 #  Создаём переменную-счётчик гласных
-for char in clean_text:
-    if char in vowels:
-        count_vowels +=1 #  3. Счётчик количества русских гласных в тексте
-print("3. Количества русских гласных в тексте:", count_vowels)
 
-repeated = {} # Создаеём dict объект
-print("4. Количество повторов каждого слова:")
-for element in words:
-    if element in repeated:
-        repeated[element] += 1 #  продолжаем считать, т.е. увеличиваем значение
-    else:
-        repeated[element] = 1 #  первый раз: создаётся элемент (ключ = слово, значение = 1)
-for element in repeated:
-    print(element, "--", repeated[element]) #  4. Выводит количество раз, которое каждое слово/число встречается в тексте.
+def russian_vowel_counter(clean_text): #  Счётчик количества русских гласных в тексте
+    vowels = "аеёиоуыэюя"
+    count_vowels = 0 #  Создаём переменную-счётчик гласных
+    for char in clean_text:
+        if char in vowels:
+            count_vowels +=1
+    print("3. Количества русских гласных в тексте:", count_vowels)
+
+
+def word_duplication_counting(words):
+    repeated = {} # Создаеём dict объект
+    print("4. Количество повторов каждого слова/числа:")
+    for element in words:
+        if element in repeated:
+            repeated[element] += 1 #  продолжаем считать, т.е. увеличиваем значение
+        else:
+            repeated[element] = 1 #  первый раз: создаётся элемент (ключ = слово, значение = 1)
+    for element in repeated:
+        print("\t", element, "--", repeated[element])
+
+
+clean_text = input_text_filter()
+words = word_count(clean_text)
+longest_word_finding(words)
+russian_vowel_counter(clean_text)
+word_duplication_counting(words)
